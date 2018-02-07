705b9a29b903a73974d52d6c268b826dd7638408
Rebecca Strauss <rstrauss127@gmail.com>
2018-02-07 02:50:40 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,8 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).delete_if {|x| x == ""}.count
+    x = self.split(/[.!?]/)
+    y = x.delete_if {|x| x == ""}
+    y.count
   end
 end
